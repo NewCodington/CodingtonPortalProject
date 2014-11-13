b2422be95f50ee1c2dd69999796ec6c90fa4343f
JAVA101_01 <JAVA101_01@ACCENTU-JOVUMMU>
2014-11-13 10:44:01 +0100

    Maria Ester

--- NewCodingtonPortalProject/src/codingtonportal/tests/PlaceDAOJUnit.java
+++ NewCodingtonPortalProject/src/codingtonportal/tests/PlaceDAOJUnit.java
@@ -1,19 +1,15 @@
 package codingtonportal.tests;
 
-import static org.junit.Assert.*;
+
 
 import java.io.IOException;
-import java.sql.Blob;
-
 import junit.framework.TestCase;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import codingtonportal.model.domain.Event;
 import codingtonportal.model.domain.Place;
-import codingtonportal.model.services.EventServiceImpl;
 import codingtonportal.model.services.PlaceServiceImpl;
 
 public class PlaceDAOJUnit extends TestCase {
@@ -45,6 +41,38 @@ public class PlaceDAOJUnit extends TestCase {
 		}
 		 
 	}
+	
+	@Test
+	public void testupdateplace() {
+		Place places = new Place (1, "Museum", "North", 1, null, "C/olvido", "Actualizado");
+		try {
+			assertTrue(place.updatePlace(places));		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
+	}
+	
+	@Test
+	public void testdeleteplace() {
+		Place places = new Place (1, "Museum", "North", 1, null, "C/olvido", "Quiero aparecer");
+		try {
+			assertTrue(place.deletePlace(places));		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
+	}
+	
+	
 
 }
 
